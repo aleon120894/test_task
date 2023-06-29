02c99a0f22dab0c4f68b491ab06c6ff13e999bcd
OLeontiev <oleontiev@C11801>
2023-06-29 19:04:39 +0300

    Add-comments-readme-refactor

--- decoder/runner.py
+++ decoder/runner.py
@@ -1,10 +1,7 @@
-import binascii
-import os
 import decoder
 
 
 if __name__ == "__main__":
-
     decoder_class = decoder.DecodeSensor()
     decoded_data = decoder_class.decoder_from_sensor('27C7011D')
     print(decoded_data)
